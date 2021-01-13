435e6be375c322d2189080c229686c4333884873
Francesco <francescodglitrico@gmail.com>
2021-01-13 11:54:48 +0000

    Improved contrast with background

--- Demo/PSO_back.hlsl
+++ Demo/PSO_back.hlsl
@@ -3,6 +3,7 @@
 #include "PSOSelector.h"
 
 #define FN(tex) ACTIVE((tex * 2 - 1))
+#define m 0.5f
 
 /**
  *  Pixel function/shader. This function must match
@@ -11,14 +12,14 @@
  */
 float4 pixel(vout i) : SV_Target
 {
-	const float4 lowEnd  = float4(0, 0, 1, 1);
-	const float4 midEnd  = float4(0, 1, 0, 1);
-	const float4 highEnd = float4(1, 0, 0, 1);
+	const float4 lowEnd  = float4(m, m, 1, 1);
+	const float4 midEnd  = float4(m, 1, m, 1);
+	const float4 highEnd = float4(1, m, m, 1);
 
 	float fn = saturate(FN(i.texcoord));
 
 	float4 lowLerp = lerp(lowEnd, midEnd, saturate(fn * 2));
 	float4 highLerp = lerp(midEnd, highEnd, saturate(fn / 2 + .5f));
 
-	return lerp(lowLerp, highLerp, fn) / 2;
+	return lerp(lowLerp, highLerp, fn);
 }
